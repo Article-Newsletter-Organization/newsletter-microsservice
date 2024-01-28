e5b09b78b4f7c1ba92401c8e848c6c69b02eb439
Lucas Eduardo Martins <93676786+LucasEduMartins@users.noreply.github.com>
2024-01-28 01:20:12 -0300

    remove unused

--- src/core/shared/EmailServicePort.ts
+++
@@ -1,7 +0,0 @@
-import { Email } from '../entities/Email';
-import { User } from '../entities/user';
-
-export interface EmailServicePort {
-  sendEmail(to: string, email: Email);
-  sendToAll(receptors: User[], email: Email);
-}
